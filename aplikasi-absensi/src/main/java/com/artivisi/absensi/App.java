260dd99c4743c18ac29796bbcef517275bfc6916
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-11 14:08:12 +0700

    template project hasil generate maven

---
+++ aplikasi-absensi/src/main/java/com/artivisi/absensi/App.java
@@ -0,0 +1,13 @@
+package com.artivisi.absensi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
