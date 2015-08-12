73beeb9f5dc9d71eaf7f7ebf1334559a580a9f50
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-12 14:23:34 +0700

    implement class member Karyawan

--- aplikasi-absensi/src/main/java/com/artivisi/absensi/App.java
+++ aplikasi-absensi/src/main/java/com/artivisi/absensi/App.java
@@ -1,13 +1,30 @@
 package com.artivisi.absensi;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	Karyawan.setJumlahKaryawan(15);
+
+        Karyawan k = new Karyawan(); // menjalankan constructor tanpa argumen
+        k.setNip("123");
+        k.setNama("Endy");
+        //k.setJumlahKaryawan(5);
+
+        Karyawan k2 = new Karyawan("456", "Adi", new Date());
+        //k2.setNip("456");
+        //k2.setNama("Adi");
+        //k2.setJumlahKaryawan(7);
+
+        System.out.println("Nama k : "+k.getNama());
+        // System.out.println("Jumlah k : "+k.getJumlahKaryawan());
+        System.out.println("Nama k2 : "+k2.getNama());
+        // System.out.println("Jumlah k2 : "+k2.getJumlahKaryawan());
+        System.out.println("Jumlah Karyawan : "+Karyawan.getJumlahKaryawan());
     }
 }
--- aplikasi-absensi/src/main/java/com/artivisi/absensi/Karyawan.java
+++ aplikasi-absensi/src/main/java/com/artivisi/absensi/Karyawan.java
@@ -1,6 +1,57 @@
 package com.artivisi.absensi;
 
+import java.util.Date;
+
 public class Karyawan {
 
+	private static Integer jumlahKaryawan = 0;
 
+	private String nip;
+	private String nama;
+	private Date tanggalLahir;
+
+	// constructor
+	public Karyawan(){ // default constructor
+		System.out.println("Constructor tanpa argumen");
+	}
+
+	public Karyawan(String nip, String nama, Date tanggalLahir){
+		System.out.println("Constructor dengan argumen");
+		this.nip = nip;
+		this.nama = nama;
+		this.tanggalLahir = tanggalLahir;
+	}
+
+	// getter dan setter
+	public static Integer getJumlahKaryawan(){
+		return jumlahKaryawan;
+	}
+
+	public static void setJumlahKaryawan(Integer x){
+		jumlahKaryawan = x;
+	}
+
+	public String getNip(){
+		return nip;
+	}
+
+	public void setNip(String nip){
+		this.nip = nip;
+	}
+
+	public String getNama(){
+		return nama;
+	}
+
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+
+	public Date getTanggalLahir(){
+		return tanggalLahir;
+	}
+
+	public void setTanggalLahir(Date tanggalLahir){
+		this.tanggalLahir = tanggalLahir;
+	}
 }
