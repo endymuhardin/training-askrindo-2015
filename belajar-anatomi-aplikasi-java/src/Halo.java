fe327e4f4c568adf921fc6adb764d3d48af9042b
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-12 10:58:13 +0700

    edit package

--- belajar-anatomi-aplikasi-java/src/Halo.java
+++ belajar-anatomi-aplikasi-java/src/Halo.java
@@ -1,7 +1,7 @@
-package id.co.askrindo.belajar.java;
+package com.artivisi.training.java.fundamental;
 
 public class Halo {
-    public static void main(String[] xx){
+    public static void main(String[] x){
         System.out.println("Halo Dunia");
     }
 }
