4e5097f64d1d6ba60d47c97c3b314c3378fce803
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-13 15:58:23 +0700

    disable schema generation & import data by sql script

--- aplikasi-absensi/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into m_karyawan (id, nip, nama, tanggal_lahir) values
-('aaa', '123', 'Endy Muhardin', '1945-08-17'),
-('bbb', '456', 'Adi', '1945-08-18');
---
+++ aplikasi-absensi/src/main/sql/data.sql
@@ -0,0 +1,4 @@
+delete from m_karyawan;
+
+insert into m_karyawan (id, nip, nama) values ('aaa', '123', 'Endy Muhardin');
+insert into m_karyawan (id, nip, nama) values ('bbb', '456', 'Adi');    
