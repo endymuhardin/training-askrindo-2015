62fba3706d1ee40daaf21a5487cf763648ce2147
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-13 09:43:12 +0700

    contoh javadoc

--- belajar-anatomi-aplikasi-java/src/Halo.java
+++ belajar-anatomi-aplikasi-java/src/Halo.java
@@ -1,6 +1,21 @@
 package com.artivisi.training.java.fundamental;
 
+/**
+ * Class untuk mendemonstrasikan aplikasi java sederhana.
+ * Class ini hanya berisi satu method, 
+ * yaitu method {@link #main(String[])}.
+ * Pada waktu dijalankan, aplikasi ini akan menampilkan 
+ * tulisan <code>Hello World</code> ke layar.
+*/
 public class Halo {
+
+	/**
+	 * method untuk menjalankan aplikasi.
+	 * Method ini akan dipanggil pada saat 
+	 * aplikasi dijalankan dari command line.
+     *
+	 * @param x opsi yang dikirimkan dari command line. Saat ini tidak digunakan.
+	*/
     public static void main(String[] x){
         System.out.println("Halo Dunia");
     }
