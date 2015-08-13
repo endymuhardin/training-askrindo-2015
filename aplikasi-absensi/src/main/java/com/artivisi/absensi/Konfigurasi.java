1187b56df19b3291c939a5433e127fc1d03174f9
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-13 16:02:02 +0700

    class ketinggalan belum dicommit

---
+++ aplikasi-absensi/src/main/java/com/artivisi/absensi/Konfigurasi.java
@@ -0,0 +1,10 @@
+package com.artivisi.absensi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Konfigurasi {
+
+}
