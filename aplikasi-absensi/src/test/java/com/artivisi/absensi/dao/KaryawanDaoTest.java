4ef1b6677c51f67f5c6875effb999f39b26237f3
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-13 14:44:14 +0700

    implement automated test

--- aplikasi-absensi/src/test/java/com/artivisi/absensi/dao/KaryawanDaoTest.java
+++ aplikasi-absensi/src/test/java/com/artivisi/absensi/dao/KaryawanDaoTest.java
@@ -1,8 +1,17 @@
 package com.artivisi.absensi.dao;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 import org.junit.runner.RunWith;
 import com.artivisi.absensi.entity.Karyawan;
+import com.artivisi.absensi.Konfigurasi;
 import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +22,40 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Konfigurasi.class)
 public class KaryawanDaoTest {
-	private KaryawanDao dao;
+	@Autowired private KaryawanDao dao;
+	@Autowired private DataSource ds;
 
-	@Test 
+	@Before
+	public void setupData() throws Exception {
+		System.out.println("====== @Before ========");
+
+		String sql = "insert into m_karyawan (nip, nama)";
+		sql += "values(?,?)";
+
+		Connection c = ds.getConnection();
+		PreparedStatement ps = c.prepareStatement(sql);
+
+		ps.setString(1, "100");
+		ps.setString(2, "Karyawan 100");
+		ps.executeUpdate();
+
+		c.close();
+	}
+	
+	@After
+	public void bersihkanData() throws Exception {
+		System.out.println("====== @After ========");
+		String sql = "delete from m_karyawan where nip in ('9999', '100')";
+
+		Connection c = ds.getConnection();
+		Statement s = c.createStatement();
+		s.executeUpdate(sql);
+		c.close();
+	}
+
+	@Test
 	public void testSimpan() throws Exception {
+		System.out.println("Test simpan");
 		Karyawan k = new Karyawan();
 		k.setNip("9999");
 		k.setNama("Karyawan Test 9999");
@@ -24,12 +63,28 @@ public class KaryawanDaoTest {
 		SimpleDateFormat formatterTanggal = new SimpleDateFormat("yyyy-MM-dd");
 		k.setTanggalLahir(formatterTanggal.parse("1945-08-17"));
 
-		//dao.save(k);
+		Long sebelumSimpan = dao.count();
+		dao.save(k);
+		Long setelahSimpan = dao.count();
+
+		Assert.assertEquals("Harusnya record bertambah satu", 
+			new Long((sebelumSimpan + 1)), new Long(setelahSimpan));
 	}
 
 	@Test 
 	public void testHitungSemua(){
-		//Long jumlahDataKaryawan = dao.count();
-		//System.out.println("Jumlah data = "+jumlahDataKaryawan);
+		System.out.println("Test hitung semua");
+		Long jumlahDataKaryawan = dao.count();
+		Assert.assertTrue("Jumlah data harusnya dua, tapi ternyata "
+			+jumlahDataKaryawan, jumlahDataKaryawan == 3);
+	}
+
+	@Test
+	public void cariNip100(){
+		Karyawan k100 = dao.findByNip("100");
+		Assert.assertNotNull("Harusnya ada data karyawan yang NIP = 100", k100);
+
+		Karyawan k101 = dao.findByNip("101");
+		Assert.assertNull("Harusnya tidak ada data karyawan yang NIP = 100", k101);
 	}
 }
