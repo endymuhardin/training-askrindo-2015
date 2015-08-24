db2396b44e33aadc56256d32fa6db8e61fb8b3a0
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-24 14:59:54 +0700

    url pattern bisa pakai php

--- belajar-aplikasi-web/src/main/java/com/artivisi/belajar/web/HaloServlet.java
+++ belajar-aplikasi-web/src/main/java/com/artivisi/belajar/web/HaloServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/halo")
+@WebServlet(urlPatterns = "/halo.php")
 public class HaloServlet extends HttpServlet {
 
     @Override
